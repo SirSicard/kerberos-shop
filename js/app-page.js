37bc45ec3dc13611e11b0f606eebeaa3ebe5aa13
Rick <rickoliwer@gmail.com>
2020-12-10 14:35:04 +0100

    page function

---
+++ js/app-page.js
@@ -0,0 +1,8 @@
+function goto(id){
+    
+    for(let article of document.querySelectorAll('article') ){
+        artivle.style.display = 'none';
+
+    }
+    document.querySelector(id).style.display = 'block';
+}
