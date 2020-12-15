4f4282b69d3bb4908545af6f85d0d246910b39ad
AlanArlukiewicz <alanarlukiewicz@gmail.com>
2020-12-15 13:36:10 +0100

    CHECKOUT script done
    testing

--- js/app-checkout.js
+++ js/app-checkout.js
@@ -4,18 +4,30 @@ function gotoCheckout(id){
     }
     document.querySelector(id).style.display = 'block';
     emptyCart();
-}
-
+ }
 // Empty cart after clicking on checkout button
 function emptyCart(){
    // let info = document.querySelector('#cart').textContent;
    // console.log(info)
-   let CartItems = document.querySelector('.list-items')
-   while(CartItems.hasChildNodes()){
-       CartItems.removeChild(CartItems.firstChild)
+   let cartItems = document.querySelectorAll('.item')
+   let cartItemsArray = Array.from(cartItems)
+   console.log(cartItemsArray);
+
+   let checkout = document.querySelector(".checkout ul")
+   for(element of cartItemsArray){
+       checkout.insertAdjacentHTML('beforeend', element.outerHTML)
    }
+   let total = document.querySelector('.total')
+   checkout.insertAdjacentHTML('beforeend', total.outerHTML)
+
+
+
+//    while(cartItems.hasChildNodes()){
+//        cartItems.removeChild(cartItems.firstChild)
+//    }
    updateTotal();
 }
-//function paymentAlert(){
-    //alert("Thanks for your purchase!");
-//}
+function paymentAlert(){
+    alert("Thanks for your purchase!");
+}
+
