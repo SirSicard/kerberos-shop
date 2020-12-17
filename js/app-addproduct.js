975aaad8575749ed8cfc7683a1f073f5af9a913a
Shakir <shakir.salman.eng@gmail.com>
2020-12-17 09:17:36 +0100

    Adding Js file

---
+++ js/app-addproduct.js
@@ -0,0 +1,41 @@
+let addProductToShopForm = queryOne(".add-product-to-shop");
+addProductToShopForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let artno = queryOne(".add-product-to-shop .product-artno").value;
+  let name = queryOne(".add-product-to-shop .product-name").value;
+  let category = queryOne(".add-product-to-shop .product-category").value;
+  let price = queryOne(".add-product-to-shop .product-price").value;
+console.log(artno , name, category, price);
+addDiv(artno , name, category, price);
+});
+
+function queryOne(selector) {
+  return document.querySelector(`${selector}`);
+}
+
+function addDiv(artno , name, category, price) {
+    
+  let div = document.createElement('div');
+  div.id = 'product';
+
+  let product = /*html*/ `<div class="content">
+    <h2>${name}</h2>
+      <ul>
+        <li class="product-artnr">Art.no: <span>${artno}</span></li>
+        <li>category: ${category}</li>
+      </ul>   
+  <button onclick="getProductInfo(this)">
+  <span>${price}</span>
+  </button>
+  </div>`;
+
+
+ div.innerHTML = product;
+  div.className = 'product';
+
+  //document.body.appendChild(div);
+let selectedDiv = document.querySelector(".products");
+
+alert(selectedDiv);
+}
+
